2422754ad09773d4e492e1817900388f1225be93
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2023-03-02 22:34:56 +0100

    Try to add PWA

--- next.config.js
+++ next.config.js
@@ -10,3 +10,29 @@ module.exports = {
   },
   presets: ["@babel/preset-env", "@babel/preset-react"],
 };
+
+const withOffline = require("next-offline");
+
+module.exports = withOffline({
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import "../styles/globals.css";
 import { Layout } from "../components";
 import React from "react";
 import { Toaster } from "react-hot-toast";
+import { Offline } from "next-offline/runtime";
 import { StateContext } from "../Context/StateContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      <Offline />
       <Toaster />
       <Layout>
         <Component {...pageProps} />
