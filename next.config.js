d78b4577c08cffec0dc59967c67060c3817d90e1
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2023-07-03 23:40:46 +0100

    trying PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
-// const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  sw: "service-worker.js",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: ["cdn.sanity.io"],
+    loader: "custom",
+  },
+  i18n: {
+    locales: ["fr"],
+    defaultLocale: "fr",
+  },
+});
+module.exports = nextConfig;
+
+//--------------------------------------------////
 
 // /** @type {import('next').NextConfig} */
+
+// const withPWA = require("next-pwa");
+// const runtimeCaching = require("next-pwa/cache");
+
 // const nextConfig = {
 //   images: {
 //     domains: ["cdn.sanity.io"],
@@ -16,26 +47,29 @@
 //     register: true,
 //     skipWaiting: true,
 //     disable: process.env.NODE_ENV === "development",
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest.json$/],
 //   },
 // });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-// module.exports = {
-//   images: {
-//     domains: ["cdn.sanity.io"],
-//     loader: "custom",
-//   },
-//   presets: ["@babel/preset-env", "@babel/preset-react"],
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
 // };
-module.exports = withPWA(nextConfig);
+
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === "development",
+
+// });
+
+// // module.exports = {
+// //   images: {
+// //     domains: ["cdn.sanity.io"],
+// //     loader: "custom",
+// //   },
+// //   presets: ["@babel/preset-env", "@babel/preset-react"],
+// // };
+// module.exports = withPWA(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,10 @@ import { Toaster } from "react-hot-toast";
 import { StateContext } from "../Context/StateContext";
 
 function MyApp({ Component, pageProps }) {
+  <meta
+    name="viewport"
+    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+  />;
   return (
     <StateContext>
       <Toaster />
